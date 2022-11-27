53d7a96283dec23058d3dc85e9324ff3ea56d5c3
sophkim <soph.b.kim@gmail.com>
2022-11-27 16:32:39 +0900

    add youtube service

--- src/App.js
+++ src/App.js
@@ -3,39 +3,18 @@ import styles from "./app.module.css";
 import SearchHeader from "./components/searchHeader/SearchHeader";
 import VideoList from "./components/VideoList/VideoList";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   const search = (query) => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyApsGiRyE9QLMWHC-i-5tO2l4s3bgdW7xI`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((items) => setVideos(items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
   };
 
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://www.googleapis.com/youtube/v3/videos?part=snippet&maxResults=25&chart=mostPopular&key=AIzaSyApsGiRyE9QLMWHC-i-5tO2l4s3bgdW7xI",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
   }, []);
 
   return (
