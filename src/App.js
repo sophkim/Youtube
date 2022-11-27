cc51c717c1bff52e21e46fd9df9239f18764c7b3
sophkim <soph.b.kim@gmail.com>
2022-11-27 16:08:39 +0900

    검색 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
+import styles from "./app.module.css";
+import SearchHeader from "./components/searchHeader/SearchHeader";
 import VideoList from "./components/VideoList/VideoList";
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const search = (query) => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyApsGiRyE9QLMWHC-i-5tO2l4s3bgdW7xI`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      )
+      .then((items) => setVideos(items))
+      .catch((error) => console.log("error", error));
+  };
 
   useEffect(() => {
     const requestOptions = {
@@ -20,7 +38,12 @@ function App() {
       .catch((error) => console.log("error", error));
   }, []);
 
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
